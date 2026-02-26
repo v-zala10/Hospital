6aaa685f363d5ee417cd34f8824a360eba1779ea
v-zala10 <vishba10@gmail.com>
2026-02-26 19:59:20 +0530

    add

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import logo from '../images/logo.webp'
+
+function Navbar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <div className="left">
+            <img src={logo} alt="Logo" width="100" />
+            <a class="navbar-brand" href="#">MediCAre
+              <p>Healthcare Solutions</p>
+            </a>
+          </div>
+
+          <div className="display">
+            <div class="collapse navbar-collapse">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Doctors</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Appointment</a>
+                </li> <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+            <div className='adminbtn'>
+
+              <button>Doctor Admin</button>
+              <button>Login</button>
+            </div>
+        </div>
+      </nav>
+
+
+    </div>
+  )
+}
+
+export default Navbar
